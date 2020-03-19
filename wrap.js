638fead5fc95fa82a44f23c35b0ba2aa63e26f67
Felicia Heiney <bhfelicia@gmail.com>
2020-03-19 12:18:29 -0400

    adds wrap spec

---
+++ wrap.js
@@ -0,0 +1,10 @@
+// function wrap(string, colNum) {
+//   let returnString = ''
+// const loopLength = string.length/colNum
+
+// for(let i = 0; i < loopLength; i++) {
+//   returnString += string.slice()
+// }
+// }
+const wrap = (line, maxLen) => "";
+module.exports = wrap;
